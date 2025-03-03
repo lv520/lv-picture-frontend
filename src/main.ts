8d3748c295c72b8b60432debb8d835a24f64d66a
lv520 <1768453362@qq.com>
2025-03-03 21:40:18 +0800

    /

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,12 @@ import Antd from "ant-design-vue";
 import "ant-design-vue/dist/reset.css";
 import '@/access.ts'
 
+import VueCropper from 'vue-cropper';
+import 'vue-cropper/dist/index.css'
 
 const app = createApp(App)
+app.use(VueCropper)
+
 app.use(Antd);
 app.use(createPinia())
 app.use(router)
